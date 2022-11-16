1468e46c65db60e44a39ba9643358fabb88b2649
Tes-program <teslimodumyiwa@gmail.com>
2022-11-16 19:57:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meme Bot v2
+This is a twitter bot that sends meme based on user keywords and can also store memes and get called back by the user.
---
+++ src/config/twitConfig.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+export const twitConfig = {
+    consumer_key:  process.env.CONSUMER_KEY,       
+    consumer_secret: process.env.CONSUMER_KEY_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
+

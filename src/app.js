cd31d640ae76a4ed15025fe6565a11c739afa480
Tes-program <teslimodumyiwa@gmail.com>
2022-12-05 09:09:28 +0100

    Add .gitignore and update services with endpoints

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 import express from "express";
 import tweet_router from "./routes/tweetrRoutes";
+import image_fetcher from "./services/imageFetcher";
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/tweet", tweet_router);
+app.use("/imageFetcher", image_fetcher)
 
 mongoose.connect(
     process.env.MONGO_URL,
--- src/services/imageFetcher.js
+++ src/services/imageFetcher.js
@@ -1,7 +1,40 @@
 import express from "express"
 import dotenv from "dotenv"
-import { awsConfig } from "../config/awsConfig"
+import { S3 } from "aws-sdk";
 const AWS = require("aws-sdk");
 
+const image_fetcher = express.Router();
+
+app.get("/imageFetcher", (req, res) => {
+    AWS.config.update({
+        accessKeyId: process.env.AWSAccessKeyID,
+        secretAccessKey: process.env.AWSSecretKey
+    });
+
+    let s3 = new AWS.S3();
+
+    async function getImage() {
+        const data = S3
+        .getObject({
+            Bucket: process.env.AWSBucket,
+            Key: "fetchedmeme/${tweet_text}"
+        })
+        .promise();
+        return data;
+    }
+
+    getImage()
+    .then((img) => {
+
+    })
+
+    function encode(data) {
+        let buf = Buffer.from(data);
+        let base64 = buf.toString("base64");
+        return base64;
+    }
+})
+
+export default image_fetcher;
 
 

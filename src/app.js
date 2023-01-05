c3dff4a1e79fca0147e5cea7a4dd42c7ca19a504
Tes-program <teslimodumyiwa@gmail.com>
2023-01-05 08:39:03 +0100

    Phase 1 done - Production

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import express from "express";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
 
 mongoose.connect(
     process.env.MONGO_URL,
